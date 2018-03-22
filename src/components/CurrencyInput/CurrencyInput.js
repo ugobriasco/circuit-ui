7ab5a92170122b3f88a9606a7d4fb456c571fac1
Julian Popov <julian.popov@sumup.com>
2018-03-22 17:49:03 +0200

    Add currency validation regex library

--- src/components/CurrencyInput/CurrencyInput.js
+++ src/components/CurrencyInput/CurrencyInput.js
@@ -1,64 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css, cx } from 'react-emotion';
+import styled, { css, cx } from 'react-emotion';
 import { withTheme } from 'emotion-theming';
 
 import IconInput from '../IconInput';
-import HtmlElement from '../HtmlElement';
-import { childrenPropType, themePropType } from '../../util/shared-prop-types';
+import { themePropType } from '../../util/shared-prop-types';
 
 const currencyBaseStyles = ({ theme }) => css`
   color: ${theme.colors.b700};
 `;
 
-const currencyStyle = ({ theme, className }) =>
-  cx(currencyBaseStyles({ theme }), className);
+const CurrencyIcon = styled('span')`
+  ${currencyBaseStyles};
+`;
 
 /**
  * SearchInput component for forms.
  */
-const CurrencyInput = ({
-  children,
-  currencyPosition,
-  currency,
-  theme,
-  ...props
-}) => (
+const CurrencyInput = ({ currencyPosition, currency, theme, ...props }) => (
   <IconInput
     iconLeft={({ className }) =>
       currencyPosition === 'left' && (
-        <HtmlElement className={currencyStyle({ theme, className })}>
-          {currency}
-        </HtmlElement>
+        <CurrencyIcon className={className}>{currency}</CurrencyIcon>
       )
     }
     iconRight={({ className }) =>
       currencyPosition === 'right' && (
-        <HtmlElement className={currencyStyle({ theme, className })}>
-          {currency}
-        </HtmlElement>
+        <CurrencyIcon className={className}>{currency}</CurrencyIcon>
       )
     }
     {...{ theme, ...props }}
-  >
-    {children}
-  </IconInput>
+  />
 );
 
-/**
- * Triggers disabled styles on the component. This is also forwarded as
- * attribute to the <input> element.
- */
 CurrencyInput.propTypes = {
   theme: themePropType.isRequired,
-  children: childrenPropType,
   currencyPosition: PropTypes.oneOf(['left', 'right']),
   textAlign: PropTypes.oneOf(['left', 'right']),
   currency: PropTypes.string
 };
 
 CurrencyInput.defaultProps = {
-  children: null,
   currencyPosition: 'right',
   textAlign: 'right',
   currency: '€'
